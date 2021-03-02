b2533cae6b95132ec76f067f76fcb59ca56cbd2b
Sneha Rajakumar <snehadr23@gmail.com>
2021-03-02 13:38:36 -0800

    React app to show list of football matches for a given year when clicked

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Year from './years/year';
+import Match from './matches/match';
+import axios from 'axios';
+  class App extends Component {
+    state = {
+      yearsList : [2011, 2012, 2013, 2014, 2015, 2016, 2017],
+      showMatches : false,
+      matches: []
+    }
+  clickYearHandler = (year) => {
+    axios.get('https://jsonmock.hackerrank.com/api/football_competitions?year=' + year)
+    .then(res => {
+      this.setState({
+        matches: res.data.data,
+        showMatches: true
+      })
+    });
+  }
 
-function App() {
+  render() {
+  let years = null;
+  let matches = null;
+
+  years = (
+    <div className = 'year-pane'>
+      {this.state.yearsList.map((year, index) => {
+        return <Year
+            year = {year}
+            click = {() => this.clickYearHandler(year)} 
+            key = {year}/>
+      })}
+    </div>
+  )
+
+  if(this.state.showMatches) {
+    if (this.state.matches.length <= 0) {
+      matches  = <p>No results found!</p>;
+    }
+    else {
+    matches = (
+      <div className = 'matches-pane'>
+        <h2>Total Matches: {this.state.matches.length}</h2>
+        {this.state.matches.map((match, index) => {
+          return <Match 
+                  name = {match.name} 
+                  winner = {match.winner}
+                  key = {match.winner}/>
+        })}
+      </div>
+    )
+      }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'App'>
+      <h1>Football Matches</h1>
+      {years}
+      {matches}
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/matches/match.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const Match = (props) => {
+    return (
+    <p>{props.name} won by {props.winner}</p>
+    )
+};
+export default Match;
